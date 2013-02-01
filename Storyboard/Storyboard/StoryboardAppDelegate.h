141931a5cce1dd32e06f0201a286a8ec6cd45de1
jordancarney <jordan.carney7@gmail.com>
2013-02-01 00:54:47 -0500

    added notes

---
+++ Storyboard/Storyboard/StoryboardAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StoryboardAppDelegate.h
+//  Storyboard
+//
+//  Created by Jordan Carney on 1/23/13.
+//  Copyright (c) 2013 Jordan Carney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryboardAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Storyboard/Storyboard/StoryboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoryboardViewController.h
+//  Storyboard
+//
+//  Created by Jordan Carney on 1/23/13.
+//  Copyright (c) 2013 Jordan Carney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryboardViewController : UIViewController
+
+@end
